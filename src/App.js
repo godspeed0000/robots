476f3930e06a9cb38db9373d25610b89ce090abf
Jack <jackwasgreat@gmail.com>
2023-08-26 16:09:28 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,113 +1,48 @@
 import React from 'react';
-import { Box, Button, Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
-import ResponsiveAppBar from './components/Header';
-import DrawerAppBar from './components/HeaderTwo';
 import Typography from '@mui/material/Typography';
 import ContactUs from './components/ContactUs';
-
-var image = require('./helpdesk.jpg');
-
+import Card from './components/Card';
+import ButtonAppBar from './components/HeaderThree';
 
 const App = () => {
   return (
     <React.Fragment>
       <CssBaseline />
-
-      <ResponsiveAppBar />
-      {/* <DrawerAppBar /> // For better view, will do later */}
+      <Box mb='40px'>
+        <ButtonAppBar />
+      </Box>
 
       <Container
         maxWidth='false'
-        sx={{ width: '100%', bgcolor: '#6d1ed4', height: '100%' }}
+        sx={{
+          width: '100%',
+          bgcolor: 'var(--bg-overlay-color)',
+          height: '100%',
+          mb: '80px',
+        }}
       >
         <Container maxWidth='xl'>
-          <Box sx={{ paddingLeft: '40px', paddingRight: '40px', mb: '50px' }}>
-            <Box
+          <Box mb='10px'>
+            <Typography
               sx={{
-                paddingLeft: '15px',
-                paddingRight: '15x',
-                width: '100%',
-                height: 'auto',
+                color: '#249723',
+                fontFamily: 'system-ui',
+                fontSize: '26px',
+                fontWeight: 800,
               }}
             >
-              <Typography
-                variant='h2'
-                component='h1'
-                sx={{
-                  fontFamily: 'system-ui',
-                  fontWeight: '600',
-                  fontSize: '3rem',
-                  paddingTop: '40px',
-                  color: 'white',
-                }}
-              >
-                What's Zelle®? Glad you asked!
-              </Typography>
-            </Box>
-            <Box pt='23px' px='15px'>
-              <Typography sx={{ color: 'white', fontFamily: 'system-ui' }}>
-                Zelle® is a great way to send money to friends and family, even
-                if they bank somewhere different than you do. That means it’s
-                super easy to pitch in or get paid back for all sorts of things
-                like coffee for your coworkers or dinner with friends.{' '}
-                <span>Zelle®</span> is already in lots of banking apps, so look
-                for it in yours today.
-              </Typography>
-            </Box>
-            <Box px='15px' mt='43px'>
-              <a href='tel:+1-8444250085' style={{ textDecoration: 'none' }}>
-                <Button
-                  sx={{
-                    backgroundColor: 'rgb(129, 93, 255)',
-                    width: '-webkit-fill-available',
-                    '&:hover': {
-                      backgroundColor: 'rgb(129, 93, 255)',
-                    },
-                  }}
-                  size='large'
-                  variant='contained'
-                >
-                  Contact Us
-                </Button>
-              </a>
-            </Box>
-            <Box pl='15px'>
-              <Typography
-                variant='h2'
-                component='h2'
-                sx={{
-                  mt: '44px',
-                  color: 'white',
-                  fontWeight: '600',
-                  fontSize: '3rem',
-                  fontFamily: 'system-ui',
-                }}
-              >
-                Any Queries? We are happy to help!
-              </Typography>
-            </Box>
+              Register and Activate your Greens-Dot online
+            </Typography>
           </Box>
-          <Box
-            sx={{ mx: '40px', width: '82%', borderRadius: '50%' }}
-            component='img'
-            alt='image not found'
-            src={image}
-          />
-          <Box pt='23px' px='15px' align='center' >
-              <Typography sx={{ color: 'white', fontFamily: 'system-ui', fontWeight: '600', fontSize: '2rem'}}>
-                Online 24/7 
-              </Typography>
-              <Typography sx={{ color: 'white', fontFamily: 'system-ui', fontWeight: '600', fontSize: '2rem'}}>
-                Customer Service
-              </Typography>
-              <Typography sx={{ color: 'white', fontFamily: 'system-ui', fontWeight: '600', fontSize: '2rem'}}>
-                +1(888)XTZ-ABCD
-              </Typography>
-            </Box>
-            <Box>
-              <ContactUs />
-            </Box>
+
+          <Box>
+            <ContactUs />
+          </Box>
+          <Box mt='40px'>
+            <Card />
+          </Box>
         </Container>
       </Container>
     </React.Fragment>
--- src/components/HeaderTwo.js
+++ src/components/HeaderTwo.js
@@ -28,8 +28,8 @@ function DrawerAppBar(props) {
 
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
+      <Typography variant='h6' sx={{ my: 2 }}>
+        User
       </Typography>
       <Divider />
       <List>
@@ -44,25 +44,26 @@ function DrawerAppBar(props) {
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar component="nav">
+      <AppBar component='nav'>
         <Toolbar>
           <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
+            color='inherit'
+            aria-label='open drawer'
+            edge='start'
             onClick={handleDrawerToggle}
             sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography
-            variant="h6"
-            component="div"
+            variant='h6'
+            component='div'
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
             MUI
@@ -76,10 +77,10 @@ function DrawerAppBar(props) {
           </Box>
         </Toolbar>
       </AppBar>
-      <Box component="nav">
+      <Box component='nav'>
         <Drawer
           container={container}
-          variant="temporary"
+          variant='temporary'
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
@@ -87,47 +88,26 @@ function DrawerAppBar(props) {
           }}
           sx={{
             display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
         </Drawer>
       </Box>
-      <Box component="main" sx={{ p: 3 }}>
+      <Box component='main' sx={{ p: 3 }}>
         <Toolbar />
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
-          fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
-          aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
-          cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem,
-          at ab sequi qui modi delectus quia corrupti alias distinctio nostrum.
-          Minima ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed
-          numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis
-          asperiores, exercitationem eius nostrum consequuntur iure aliquam itaque,
-          assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus
-          soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae
-          ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque
-          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem!
-          Cumque, eligendi unde aliquid minus quis sit debitis obcaecati error,
-          delectus quo eius exercitationem tempore. Delectus sapiente, provident
-          corporis dolorum quibusdam aut beatae repellendus est labore quisquam
-          praesentium repudiandae non vel laboriosam quo ab perferendis velit ipsa
-          deleniti modi! Ipsam, illo quod. Nesciunt commodi nihil corrupti cum non
-          fugiat praesentium doloremque architecto laborum aliquid. Quae, maxime
-          recusandae? Eveniet dolore molestiae dicta blanditiis est expedita eius
-          debitis cupiditate porro sed aspernatur quidem, repellat nihil quasi
-          praesentium quia eos, quibusdam provident. Incidunt tempore vel placeat
-          voluptate iure labore, repellendus beatae quia unde est aliquid dolor
-          molestias libero. Reiciendis similique exercitationem consequatur, nobis
-          placeat illo laudantium! Enim perferendis nulla soluta magni error,
-          provident repellat similique cupiditate ipsam, et tempore cumque quod! Qui,
-          iure suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
-          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore commodi
-          reprehenderit rerum reiciendis! Quidem alias repudiandae eaque eveniet
-          cumque nihil aliquam in expedita, impedit quas ipsum nesciunt ipsa ullam
-          consequuntur dignissimos numquam at nisi porro a, quaerat rem repellendus.
-          Voluptates perspiciatis, in pariatur impedit, nam facilis libero dolorem
-          dolores sunt inventore perferendis, aut sapiente modi nesciunt.
+        <Typography
+          sx={{
+            color: '#249723',
+            fontFamily: 'raleway, sans-serif',
+            fontSize: '26px',
+            fontWeight: 800,
+          }}
+        >
+          Register and Activate your Greens-Dot online
         </Typography>
       </Box>
     </Box>
