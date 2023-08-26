1b06531028e834e91541e83566917b28be8888e4
Jack <jackwasgreat@gmail.com>
2023-08-26 16:02:03 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -1,50 +1,117 @@
-import 'bulma/css/bulma.css';
-import ProfileCard from './ProfileCard';
-import AlexaImage from './images/alexa.png';
-import CortanaImage from './images/cortana.png';
-import SiriImage from './images/siri.png';
+import React from 'react';
+import { Box, Button, Container } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import ResponsiveAppBar from './components/Header';
+import DrawerAppBar from './components/HeaderTwo';
+import Typography from '@mui/material/Typography';
+import ContactUs from './components/ContactUs';
 
-function App() {
+var image = require('./helpdesk.jpg');
+
+
+const App = () => {
   return (
-    <div>
-      <section className="hero is-primary">
-        <div className="hero-body">
-          <p className="title">Personal Digital Assistants</p>
-        </div>
-      </section>
+    <React.Fragment>
+      <CssBaseline />
 
-      <div className="container">
-        <section className="section">
-          <div className="columns">
-            <div className="column is-4">
-              <ProfileCard
-                title="Alexa"
-                handle="@alexa99"
-                image={AlexaImage}
-                description="Alexa was created by Amazon and helps you resolves query."
-              />
-            </div>
-            <div className="column is-4">
-              <ProfileCard
-                title="Cortana"
-                handle="@cortana32"
-                image={CortanaImage}
-                description="Cortana was made by Microsoft. Who knows what customer support it provides?"
-              />
-            </div>
-            <div className="column is-4">
-              <ProfileCard
-                title="Siri"
-                handle="@siri01"
-                image={SiriImage}
-                description="Siri was made by Apple and is being phased out, Debit cards you need"
-              />
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
+      <ResponsiveAppBar />
+      {/* <DrawerAppBar /> // For better view, will do later */}
+
+      <Container
+        maxWidth='false'
+        sx={{ width: '100%', bgcolor: '#6d1ed4', height: '100%' }}
+      >
+        <Container maxWidth='xl'>
+          <Box sx={{ paddingLeft: '40px', paddingRight: '40px', mb: '50px' }}>
+            <Box
+              sx={{
+                paddingLeft: '15px',
+                paddingRight: '15x',
+                width: '100%',
+                height: 'auto',
+              }}
+            >
+              <Typography
+                variant='h2'
+                component='h1'
+                sx={{
+                  fontFamily: 'system-ui',
+                  fontWeight: '600',
+                  fontSize: '3rem',
+                  paddingTop: '40px',
+                  color: 'white',
+                }}
+              >
+                What's Zelle®? Glad you asked!
+              </Typography>
+            </Box>
+            <Box pt='23px' px='15px'>
+              <Typography sx={{ color: 'white', fontFamily: 'system-ui' }}>
+                Zelle® is a great way to send money to friends and family, even
+                if they bank somewhere different than you do. That means it’s
+                super easy to pitch in or get paid back for all sorts of things
+                like coffee for your coworkers or dinner with friends.{' '}
+                <span>Zelle®</span> is already in lots of banking apps, so look
+                for it in yours today.
+              </Typography>
+            </Box>
+            <Box px='15px' mt='43px'>
+              <a href='tel:+1-8444250085' style={{ textDecoration: 'none' }}>
+                <Button
+                  sx={{
+                    backgroundColor: 'rgb(129, 93, 255)',
+                    width: '-webkit-fill-available',
+                    '&:hover': {
+                      backgroundColor: 'rgb(129, 93, 255)',
+                    },
+                  }}
+                  size='large'
+                  variant='contained'
+                >
+                  Contact Us
+                </Button>
+              </a>
+            </Box>
+            <Box pl='15px'>
+              <Typography
+                variant='h2'
+                component='h2'
+                sx={{
+                  mt: '44px',
+                  color: 'white',
+                  fontWeight: '600',
+                  fontSize: '3rem',
+                  fontFamily: 'system-ui',
+                }}
+              >
+                Any Queries? We are happy to help!
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{ mx: '40px', width: '82%', borderRadius: '50%' }}
+            component='img'
+            alt='image not found'
+            src={image}
+          />
+          <Box pt='23px' px='15px' align='center' >
+              <Typography sx={{ color: 'white', fontFamily: 'system-ui', fontWeight: '600', fontSize: '2rem'}}>
+                Online 24/7 
+              </Typography>
+              <Typography sx={{ color: 'white', fontFamily: 'system-ui', fontWeight: '600', fontSize: '2rem'}}>
+                Customer Service
+              </Typography>
+              <Typography sx={{ color: 'white', fontFamily: 'system-ui', fontWeight: '600', fontSize: '2rem'}}>
+                +1(888)XTZ-ABCD
+              </Typography>
+            </Box>
+            <Box>
+              <ContactUs />
+            </Box>
+        </Container>
+      </Container>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,43 +2,134 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import greensLogo from '../greenCircle.jpg'
+import MenuItem from '@mui/material/MenuItem';
 
 const pages = ['How it works', 'Get Started', 'FAQ'];
 
 function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    <AppBar position='static' sx={{ backgroundColor: 'white' }}>
+    <AppBar position='static' sx={{ backgroundColor: '#6d1ed4' }}>
       <Container maxWidth='xl'>
         <Toolbar disableGutters>
           <Typography
             variant='h6'
-            // noWrap
-            // component='a'
-            // href='/'
+            noWrap
+            component='a'
+            href='/'
             sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
               fontFamily: 'monospace',
-              fontWeight: '900',
-              color: 'black',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
               textDecoration: 'none',
             }}
           >
-            GreensDot
+            Zelle
           </Typography>
 
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign='center'>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant='h5'
+            noWrap
+            component='a'
+            href=''
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+            }}
+          >
+            <img src='https://img1.wsimg.com/isteam/ip/37adf295-dd4f-4132-b817-2ddccacd4893/ge.jpeg/:/rs=w:90,h:70' />
+          </Typography>
           {/* <Box
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+            }}
+            component='img'
+            alt='Logo'
+            src='https://img1.wsimg.com/isteam/ip/37adf295-dd4f-4132-b817-2ddccacd4893/ge.jpeg/:/rs=w:90,h:70'
+          /> */}
+
+          <Box
             sx={{
               flexGrow: 1,
               justifyContent: 'flex-end',
               display: { xs: 'none', md: 'flex' },
             }}
           >
-            {}
-          </Box> */}
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
